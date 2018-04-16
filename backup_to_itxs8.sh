54ad3e05d701dbfcc660d2fc1e07349b82951dc4
y2k <git@y2kbugger.com>
2018-04-15 22:44:29 -0400

    Add ability to do dry run, detect android

--- backup_to_itxs8.sh
+++ backup_to_itxs8.sh
@@ -3,15 +3,44 @@
 bakroot="/media/itxs8_fs"
 bakdatetime=$(date +%Y-%m-%dT%H:%M:%S%z)
 
-data="${bakroot}/$HOSTNAME"
-snap="${bakroot}/bak/$HOSTNAME/${bakdatetime}"
+case "$HOSTNAME" in
+  15r)
+    backupname=$HOSTNAME
+    ;;
+  *)
+    echo "Is this running from pix2?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) backupname="pix2"; break;;
+            No ) exit;;
+        esac
+    done
+    ;;
+esac
+
+data="${bakroot}/$backupname"
+snap="${bakroot}/bak/$backupname/${bakdatetime}"
 
 shopt -s expand_aliases
 alias rsynclog="ssh y2k@192.168.1.228 \"cat >> $snap.log\""
 
+
+echo "Dry run?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) dry="--dry-run"; break;;
+        No ) dry=""; break;;
+    esac
+done
+
 # backup current state
-rsync -aAXvih --delete --progress /home/y2k/ y2k@192.168.1.228:$data | rsynclog
+rsync -aAXvihx ${dry} --delete --progress /home/y2k/ y2k@192.168.1.228:$data | rsynclog
 sync
+
+
+if [ -r "/storage/emulated/0" ]; then
+    echo "we are on android and can bak up internal sd"
+fi
 
 # make a read only snapshot
 ssh y2k@192.168.1.228 "sudo btrfs subvolume snapshot -r $data $snap && sync"
